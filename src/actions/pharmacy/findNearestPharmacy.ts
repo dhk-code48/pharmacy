4a1e92b274cf3023384c95420e5caf700cf48045
dhk-code48 <dhk.code48@gmail.com>
2024-09-24 23:20:22 +0545

    Fix Build Error

--- src/actions/pharmacy/findNearestPharmacy.ts
+++ src/actions/pharmacy/findNearestPharmacy.ts
@@ -1,10 +1,6 @@
 "use server";
 
 import { prisma } from "@/lib/db";
-import { haversineDistance } from "@/lib/utils";
-import { Pharmacy } from "@prisma/client";
-import { gerNearestPharmacy } from "@prisma/client/sql";
-import { Sql } from "@prisma/client/runtime/library";
 import { NearestPharmacy } from "@/types";
 
 export default async function findNearestPharmacies(userLat: number, userLng: number, numPharmacies: number) {
